74080bb469deb2d198b982816120ebcf82ee95f4
Girlc7442 <girlc7442@wrdsb.ca>
2020-11-03 17:17:13 +0000

    finished with comments

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
 /**
- *
- * @author 
+ * Tells the user if a given number is harshad
+ * @author Cole Girling
  */
 public class Main {
+
+  public static boolean isNiven(int number){
+    //initializes the accumulator at 0
+    int sumOfDigits = 0;
+    
+    //saves the original number
+    int digits = number;
+    
+    //runs while there is more than one digit in the number
+    while(digits >= 10){
+      digits = digits % 10;
+
+      sumOfDigits = sumOfDigits + digits;
+    }
+  
+    //determines if the number is harshad and will return either true or false depending on if it is
+    if(number % sumOfDigits >= 1){
+      return false;
+    }else{
+      return true;
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //calls the method and inserts the number into the method
+    boolean isHarshadNumber = isNiven(14);
+    
+    //prints true or false to the screen 
+    System.out.println(isHarshadNumber);
     
   }
 }
